815078aab473a45e8e980c63f4fcbc5158162de5
titevar <59051464+titevar@users.noreply.github.com>
2021-07-09 23:00:23 +0400

    testing svg

--- src/components/About.js
+++ src/components/About.js
@@ -1,26 +1,29 @@
-import React from "react"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react';
+import { FaGithub, FaHtml5 } from 'react-icons/fa';
+import html5 from '../media/html5.svg';
+import css3 from '../media/css3.svg';
+import javascript from '../media/javascript.svg';
 
- 
+import '../css/about.css'
+
 function About(props) {
-  return (
-    <section className="animation-container" id="about">
-   
-    <div id="demo"></div>
-  <div id="myDIV" width="300px">asdasd</div>
-      <h1>Programming languages, frameworks and technologies I am working with:</h1>
-      <i className="fab fa-html5"></i>
-      <p>HTML, CSS, Javascript</p>
-      <div>
-    <FontAwesomeIcon icon="check-square" />
-    Your <FontAwesomeIcon icon="coffee" /> is hot and ready!
-    <FontAwesomeIcon icon={['fab', 'apple']} />
-    <FontAwesomeIcon icon={['fab', 'microsoft']} />
-    <FontAwesomeIcon icon={['fab', 'google']} />
-  </div>
-     
-    </section>
-  )
+	return (
+		<section className="animation-container" id="about">
+			<div id="demo" />
+			<div id="myDIV" width="300px">
+				asdasd
+			</div>
+			<h1>Programming languages, frameworks and technologies I am working with:</h1>
+			<i className="fab fa-html5" />
+			<p>HTML, CSS, Javascript</p>
+			<FaGithub size="170" />
+			<FaHtml5 size="170" />
+			<img className="logoImage" src={html5} alt="html5" />
+      <img className="logoImage" src={css3} alt="html5" />
+      <img className="logoImage" src={javascript} alt="hl5" />
+			<div />
+		</section>
+	);
 }
- 
+
 export default About;
